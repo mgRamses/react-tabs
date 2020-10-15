32023c06da83f6bb0528c8ccc69ef0c801de5e0b
Ram <mg.ramses@gmail.com>
2020-10-15 16:53:34 -0500

    docs agregado

---
+++ docs/precache-manifest.f69f3bea700d284b314b24d9cd6032f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afbab99b1a8db7feade34377575d764",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dfe6705a7479091bfb5",
+    "url": "/static/css/main.b8cd7ddb.chunk.css"
+  },
+  {
+    "revision": "932e0635dc84794fd52b",
+    "url": "/static/js/2.a030575a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a030575a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dfe6705a7479091bfb5",
+    "url": "/static/js/main.7d29d52f.chunk.js"
+  },
+  {
+    "revision": "e5d628ff18d24e18b597",
+    "url": "/static/js/runtime-main.af0cddbb.js"
+  }
+]);
